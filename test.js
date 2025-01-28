113a81706a1dcb6d7926a46e57e66624e1568b33
ZsJuhaszZalan <zsiros-juhasz.zalan@diak.szbi-pg.hu>
2025-01-28 09:26:20 +0100

    kép

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("Hello World")
+
+
+function kep_beilleszt() {
+    document.getElementById("kep_helye").innerHTML = "<img src='gay.jpg'>;"
+}
+
+function kep_torles() {
+    document.getElementById("kep_helye").innerHTML = "<img src=''>;"
+}
