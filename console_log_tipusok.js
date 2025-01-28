2aa337cbfc464eb3e96baaab6e1003bab51a70b8
ZsJuhaszZalan <zsiros-juhasz.zalan@diak.szbi-pg.hu>
2025-01-28 09:45:20 +0100

    consolok

---
+++ console_log_tipusok.js
@@ -0,0 +1,10 @@
+console.log("Ez egy normál üzenet.")
+console.warn("Ez egy figyelmeztető üzenet.")
+console.error("Ez egy hibauzenet.");
+
+console.table(
+    [
+        {név: "Anna", kor: 25},
+        {név: "Béla", kor: 30},
+    ]
+)
